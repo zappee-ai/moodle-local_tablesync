6624bf84a89c0ada719e754c2a30899696ded8a3
Robert Nay <rsn.nay@gmail.com>
2021-02-08 10:55:30 -0700

    Revert version bump

--- version.php
+++ version.php
@@ -25,6 +25,6 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 2021020501; // The current plugin version (Date: YYYYMMDDXX).
+$plugin->version = 2021012002; // The current plugin version (Date: YYYYMMDDXX).
 $plugin->requires = 2020061500; // Requires this Moodle version.
 $plugin->component = 'local_tablesync'; // Full name of the plugin (used for diagnostics).
